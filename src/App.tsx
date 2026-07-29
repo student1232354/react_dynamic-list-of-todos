052975dd27ece7f09319a7c9da095681090afcdb
student1232354 <tarasromanenko33@gmail.com>
2026-07-29 20:33:33 +0300

    first update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,40 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [array, setArray] = useState<Todo[]>([]);
+  const [loading, setloading] = useState(true);
+  const [newarray, setnewarray] = useState<Todo | null>(null);
+
+  const [matching, setmatching] = useState('all');
+  const [titling, settitling] = useState('');
+
+  useEffect(() => {
+    getTodos()
+      .then(fetchedTodos => {
+        setArray(fetchedTodos);
+      })
+      .finally(() => {
+        setloading(false);
+      });
+  }, []);
+
+  const PreparedArray = array.filter(obj => {
+    const fExam =
+      matching === 'all' ||
+      (matching === 'active' && !obj.completed) ||
+      (matching === 'completed' && obj.completed);
+
+    const sExam = obj.title
+      .toLowerCase()
+      .includes(titling.toLowerCase().trim());
+
+    return fExam && sExam;
+  });
+
   return (
     <>
       <div className="section">
@@ -17,18 +49,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                setmatching={setmatching}
+                matching={matching}
+                titling={titling}
+                settitling={settitling}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  array={PreparedArray}
+                  selectedTodo={newarray}
+                  onselect={setnewarray}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {newarray && (
+        <TodoModal selectedobj={newarray} OnClose={() => setnewarray(null)} />
+      )}
+      ;
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Loader } from '../Loader';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  todo: Todo;
+  onClose: () => void;
+};
+
+export const TodoModal: React.FC<Props> = ({ selectedobj, OnClose }) => {
+  const [newobj, setobj] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    getUser(selectedobj.userId)
+      .then(fetchedUser => setobj(fetchedUser))
+      .finally(() => setLoading(false));
+  }, [selectedobj.userId]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {true ? (
+      {loading ? (
         <Loader />
       ) : (
         <div className="modal-card">
@@ -15,25 +33,34 @@ export const TodoModal: React.FC = () => {
               className="modal-card-title has-text-weight-medium"
               data-cy="modal-header"
             >
-              Todo #2
+              Todo #{selectedobj.id}
             </div>
 
             {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <button type="button" className="delete" data-cy="modal-close" />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => OnClose(null)}
+            />
           </header>
 
           <div className="modal-card-body">
             <p className="block" data-cy="modal-title">
-              quis ut nam facilis et officia qui
+              {selectedobj.title}
             </p>
 
             <p className="block" data-cy="modal-user">
               {/* <strong className="has-text-success">Done</strong> */}
-              <strong className="has-text-danger">Planned</strong>
+              {selectedobj.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
 
               {' by '}
 
-              <a href="mailto:Sincere@april.biz">Leanne Graham</a>
+              {newobj && <a href={`mailto:${newobj.email}`}>{newobj.name}</a>}
             </p>
           </div>
         </div>
